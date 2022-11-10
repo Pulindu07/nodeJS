b7ef6629abd7169402b5ae7c54cf501860a39476
Pulindu de Silva <pulindu@Pulindus-MacBook-Air.local>
2022-11-10 16:55:17 +0530

    Try query with request

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const { log } = require("console")
 const express = require("express")
 const path = require("path")
+const { hasUncaughtExceptionCaptureCallback } = require("process")
 
 const app = express()
 
@@ -7,6 +9,17 @@ app.get("/",(req,res)=>{
     res.status(200).sendFile(path.join(__dirname,"./public/index.html"))
 })
 
+app.get("/api",(req,res)=>{
+    res.send("<button><a href='http://hn.algolia.com/api/v1/search?query=foo&tags=story'>Click</a></button>")
+})
+app.get("http://hn.algolia.com/api/v1/search?query=foo&tags=story",(req,res)=>{
+    res.fetch("http://hn.algolia.com/api/v1/search?query=foo&tags=story")
+})
+
+app.get("/api/v2/query",(req,res)=>{
+    console.log(req.query)
+    res.status(200).sendFile(path.join(__dirname,"./public/index.html"))
+})
 app.all("*",(req,res)=>{
     res.status(404).send("Page is not available!")
 })
